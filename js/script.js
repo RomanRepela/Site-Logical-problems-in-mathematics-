71f93137ae0fde9fda3cbaa5bc264e57f09af101
RomanRepela <121348374+RomanRepela@users.noreply.github.com>
2023-01-12 19:20:17 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$('#title-block1').click(function () {
+  $('#sub-block1').toggle(1000);
+});
+
+$('#title-block2').click(function () {
+  $('#sub-block2').toggle(1000);
+});
+
+$('#title-block3').click(function () {
+  $('#sub-block3').toggle(1000);
+});
+
+$('#title-block4').click(function () {
+  $('#sub-block4').toggle(1000);
+});
